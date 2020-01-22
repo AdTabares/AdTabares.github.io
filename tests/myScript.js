c92eb984f720847a9c3cbb75e34cc68091a585aa
AdTabares <55292441+AdTabares@users.noreply.github.com>
2020-01-22 13:53:48 -0500

    Update myScript.js

--- tests/myScript.js
+++ tests/myScript.js
@@ -15,3 +15,16 @@ while(beginning > 0)
     beginning = Math.random () * 11;
      beginning--;
    }
+function myLoop(){
+var end = document.getElementById("end").value;
+for(var i = 0; i <= end; i++)
+  {
+    document.write('<p>' + i + '</p>');
+  }
+}
+document.getElementById("start").value;
+for(var i = "start"; i <= end; i++)
+  {
+    document.write('<p>' + i + '</p>');
+  }
+
